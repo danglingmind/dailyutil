7e91f9b412bfc1e2a46a6b40e7da16976b10ec6b
Prateek Reddy <praddy@amazon.com>
2024-08-09 19:49:36 +0530

    creator name added in footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,18 @@
+import { inter, teko } from "../fonts";
+
 export default function Footer() {
   return (
-    <footer className="w-full bg-base-content text-primary-content p-4 text-center">
+    <footer
+      className={
+        `${inter.className} ` +
+        "w-full bg-base-content text-primary-content p-4 text-center"
+      }
+    >
       &copy; {new Date().getFullYear()} contertverse.in
+      <div className="text-center m-1">
+        Made with <span className="text-xl">☕</span> by{" "}
+        <span className={teko.className}>Prateek Reddy</span>
+      </div>
     </footer>
   );
 }
