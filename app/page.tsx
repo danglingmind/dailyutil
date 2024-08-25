94495bff3a73b6b80e279437af9b4b4ff1db24ec
Prateek Reddy <praddy@amazon.com>
2024-08-25 19:10:51 +0530

    home page text removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,10 @@ import Spline from "@splinetool/react-spline/next";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col items-center gap-5 p-24">
+    <main className="flex w-full min-h-screen flex-col items-center gap-5">
       <div>
         <Spline scene="https://prod.spline.design/61ficUp7YUaVYv-y/scene.splinecode" />
       </div>
-      <div className="text-2xl">Welcome to the ultimate converter tool.</div>
-      <div className="text-xl">Bookmark it</div>
-      <div className="text-3xl">Happy conversion</div>
     </main>
   );
 }
