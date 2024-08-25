4cdb06d2ed35d48edece70cd66f05a2234ad6b3c
Prateek Reddy <praddy@amazon.com>
2024-08-25 17:47:49 +0530

    formatter improved with code viewer

--- app/(site)/jsonformat/page.tsx
+++ app/(site)/jsonformat/page.tsx
@@ -1,33 +1,61 @@
 "use client";
 
 import { useState } from "react";
+import { CopyBlock, dracula } from "react-code-blocks";
 
 export default function JsonFormat() {
   const [inputText, setInputText] = useState("");
   const [formattedText, setFormattedText] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   return (
-    <div className="flex flex-col gap-3 w-full h-lvh p-5">
-      <textarea
-        className="textarea textarea-bordered w-3/4 p-2 bg-blue-800 text-white"
-        style={{ margin: "0 auto", minHeight: "80vh" }}
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <button
-        className="btn btn-primary w-fit"
-        style={{ margin: "0 auto" }}
-        onClick={() => {
-          try {
-            setFormattedText(JSON.stringify(JSON.parse(inputText), null, 2));
-            setInputText(JSON.stringify(JSON.parse(inputText), null, 2));
-          } catch (error) {
-            alert("Invalid JSON format");
-          }
-        }}
-      >
-        Format
-      </button>
+    <div className="flex flex-col gap-3 w-full p-5">
+      <div className="flex gap-3 w-full justify-center">
+        <button
+          className="btn btn-primary w-fit btn-sm"
+          onClick={() => {
+            try {
+              setFormattedText(JSON.stringify(JSON.parse(inputText), null, 2));
+              setInputText(JSON.stringify(JSON.parse(inputText), null, 2));
+              setSubmitted(true);
+            } catch (error) {
+              alert("Invalid JSON format");
+            }
+          }}
+        >
+          Format
+        </button>
+        <button
+          className="btn btn-warning w-fit btn-sm"
+          onClick={() => setSubmitted(false)}
+        >
+          Edit
+        </button>
+      </div>
+      {!submitted ? (
+        <textarea
+          className="textarea textarea-bordered w-1/2 p-2"
+          style={{
+            margin: "0 auto",
+            minHeight: "70vh",
+            background: "#282A36",
+            color: "#F8F8F2",
+          }}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+      ) : (
+        <div className="w-1/2" style={{ margin: "0 auto" }}>
+          <CopyBlock
+            wrapLongLines
+            text={formattedText}
+            language={"jsx"}
+            showLineNumbers={true}
+            theme={dracula}
+            codeBlock
+          />
+        </div>
+      )}
     </div>
   );
 }
