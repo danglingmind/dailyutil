554a87edf0a464b591ae320da3a0448eb536ce6b
Prateek Reddy <praddy@amazon.com>
2024-08-25 17:22:58 +0530

    homepage fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-5 p-24">
+    <main className="flex w-full min-h-screen flex-col items-center gap-5 p-24">
       <div className="text-2xl">Welcome to the ultimate converter tool.</div>
       <div className="text-xl">Bookmark it</div>
       <div className="text-3xl">Happy conversion</div>
